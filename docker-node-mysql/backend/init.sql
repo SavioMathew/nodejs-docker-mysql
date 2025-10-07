cd565a2febc0d049feee3757864991f3267e3aa3
Savio Mathew <savio.mathew6@gmail.com>
2025-10-08 00:39:27 +0530

    Delete docker-node-mysql directory

--- docker-node-mysql/backend/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS mydb;
-USE mydb;
-CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT PRIMARY KEY, message VARCHAR(255));
-INSERT INTO messages (message) VALUES ('Hello from MySQL (Ubuntu container)!');
-
